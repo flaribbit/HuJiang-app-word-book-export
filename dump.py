e48cd8616bbe55b17f4baab5b88fe8b2820c7b87
flaribbit <1149761294@qq.com>
2021-07-09 11:39:44 +0800

    优化代码

--- dump.py
+++ dump.py
@@ -3,25 +3,25 @@ import base64
 import os
 import re
 
-def decode(s):
-    bytes=bytearray(base64.b64decode(s))
+
+def decode(s: str):
+    bytes = bytearray(base64.b64decode(s))
     for i in range(len(bytes)):
-        bytes[i]=255^bytes[i]
+        bytes[i] = 255 ^ bytes[i]
     return bytes.decode("utf8")
 
-def dumpBook(s,out):
-    file=open(out,"w",encoding="utf8")
-    conn=sqlite3.connect(s)
-    result=conn.execute("SELECT word,word_phonetic,word_def FROM book_word")
-    for row in result:
-        # print(row[0],row[1],decode(row[2]),sep="\t")
-        file.write("%s\t%s\t%s\n"%(row[0],row[1],decode(row[2])))
-    file.close()
-    result.close()
-    conn.close()
+
+def dumpBook(dbfile: str, out: str):
+    with open(out, "w", encoding="utf8") as file, sqlite3.connect(dbfile) as conn:
+        result = conn.execute(
+            "SELECT word, word_phonetic, word_def FROM book_word")
+        for row in result:
+            file.write("{}\t{}\t{}\n".format(row[0], row[1], decode(row[2])))
+        result.close()
+
 
 for item in os.listdir():
-    result=re.match("(iWord_book_word.*?)\\.db",item)
+    result = re.match("(iWord_book_word.*?)\\.db", item)
     if result:
-        dumpBook(item,result.group(1)+".txt")
+        dumpBook(item, result.group(1)+".txt")
         # print(result)
